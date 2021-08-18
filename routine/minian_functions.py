0bdf6a9fde7d2bf4ed10b4592950ed40a9173752
phildong <zhe.dong@icahn.mssm.edu>
2021-08-18 16:23:18 -0400

    add functions to write video and open minian

--- routine/minian_functions.py
+++ routine/minian_functions.py
@@ -1,9 +1,10 @@
 import os
 import shutil
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Callable, Union
 
 import dask.array as darr
+import ffmpeg
 import numpy as np
 import xarray as xr
 
@@ -70,3 +71,48 @@ def save_minian(
         arr = xr.open_zarr(fp)[var.name]
         arr.data = darr.from_zarr(os.path.join(fp, var.name), inline_array=True)
     return arr
+
+
+def write_video(
+    arr: xr.DataArray,
+    fname: str,
+    options={"crf": "18", "preset": "ultrafast"},
+) -> str:
+    arr = arr.clip(0, 255).astype(np.uint8)
+    w, h = arr.sizes["width"], arr.sizes["height"]
+    process = (
+        ffmpeg.input("pipe:", format="rawvideo", pix_fmt="gray", s="{}x{}".format(w, h))
+        .filter("pad", int(np.ceil(w / 2) * 2), int(np.ceil(h / 2) * 2))
+        .output(fname, pix_fmt="yuv420p", vcodec="libx264", r=30, **options)
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
+    for blk in arr.data.blocks:
+        process.stdin.write(np.array(blk).tobytes())
+    process.stdin.close()
+    process.wait()
+    return fname
+
+
+def open_minian(
+    dpath: str, post_process: Optional[Callable] = None, return_dict=False
+) -> Union[dict, xr.Dataset]:
+    if os.path.isfile(dpath):
+        ds = xr.open_dataset(dpath).chunk()
+    elif os.path.isdir(dpath):
+        dslist = []
+        for d in os.listdir(dpath):
+            arr_path = os.path.join(dpath, d)
+            if os.path.isdir(arr_path):
+                arr = list(xr.open_zarr(arr_path).values())[0]
+                arr.data = darr.from_zarr(
+                    os.path.join(arr_path, arr.name), inline_array=True
+                )
+                dslist.append(arr)
+        if return_dict:
+            ds = {d.name: d for d in dslist}
+        else:
+            ds = xr.merge(dslist, compat="no_conflicts")
+    if (not return_dict) and post_process:
+        ds = post_process(ds, dpath)
+    return ds
