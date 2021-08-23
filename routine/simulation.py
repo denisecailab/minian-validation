9965fe03e943cd3aaa67982fd364edabcf387077
phildong <zhe.dong@icahn.mssm.edu>
2021-08-23 16:22:52 -0400

    switch to dask-based simulation

--- routine/simulation.py
+++ routine/simulation.py
@@ -2,8 +2,10 @@
 import os
 from typing import List
 
+import dask.array as darr
 import numba as nb
 import numpy as np
+import sparse
 import xarray as xr
 from numpy import random
 from scipy.stats import multivariate_normal
@@ -91,6 +93,8 @@ def simulate_data(
     mo_stps: List = [0],
     mo_pstp: List = [1],
     cent=None,
+    zero_thres=1e-8,
+    chk_size=1000,
 ):
     ff, hh, ww = (
         dims["frame"],
@@ -98,12 +102,14 @@ def simulate_data(
         dims["width"],
     )
     shifts = xr.DataArray(
-        random_walk(ff, ndim=2, stp=mo_stps, p_stp=mo_pstp),
+        darr.from_array(
+            random_walk(ff, ndim=2, stp=mo_stps, p_stp=mo_pstp), chunks=(chk_size, -1)
+        ),
         dims=["frame", "shift_dim"],
         coords={"frame": np.arange(ff), "shift_dim": ["height", "width"]},
         name="shifts",
     )
-    pad = np.absolute(shifts).max().astype(int).values
+    pad = np.absolute(shifts).max().values.item()
     if cent is None:
         cent = np.stack(
             [
@@ -112,37 +118,21 @@ def simulate_data(
             ],
             axis=1,
         )
-    A = xr.DataArray(
-        gauss_cell(
-            2 * pad + hh,
-            2 * pad + ww,
-            sz_mean=sz_mean,
-            sz_sigma=sz_sigma,
-            sz_min=sz_min,
-            cent=cent,
-        ),
-        dims=["unit_id", "height", "width"],
-        coords={
-            "height": np.arange(2 * pad + hh),
-            "width": np.arange(2 * pad + ww),
-            "unit_id": np.arange(ncell),
-        },
-        name="A",
+    A = gauss_cell(
+        2 * pad + hh,
+        2 * pad + ww,
+        sz_mean=sz_mean,
+        sz_sigma=sz_sigma,
+        sz_min=sz_min,
+        cent=cent,
+    )
+    A = darr.from_array(
+        sparse.COO.from_numpy(np.where(A > zero_thres, A, 0)), chunks=-1
     )
     traces = [exp_trace(ff, tmp_pfire, tmp_tau_d, tmp_tau_r) for _ in range(len(cent))]
-    C = xr.DataArray(
-        np.stack([t[0] for t in traces]),
-        dims=["unit_id", "frame"],
-        coords={"unit_id": np.arange(ncell), "frame": np.arange(ff)},
-        name="C",
-    )
-    S = xr.DataArray(
-        np.stack([t[1] for t in traces]),
-        dims=["unit_id", "frame"],
-        coords={"unit_id": np.arange(ncell), "frame": np.arange(ff)},
-        name="S",
-    )
-    Y = C.dot(A).rename("Y") * sig_scale
+    C = darr.from_array(np.stack([t[0] for t in traces]), chunks=(-1, chk_size))
+    S = darr.from_array(np.stack([t[1] for t in traces]), chunks=(-1, chk_size))
+    Y = darr.tensordot(C, A, axes=[(0,), (0,)]) * sig_scale
     if bg_nsrc > 0:
         cent_bg = np.stack(
             [
@@ -159,41 +149,57 @@ def simulate_data(
             sz_min=sz_min,
             cent=cent_bg,
         )
-        C_bg = random_walk(ff, ndim=bg_nsrc, norm=True)
-        Y_bg = xr.DataArray(
-            np.tensordot(C_bg, A_bg, axes=1),
-            dims=["frame", "height", "width"],
-            coords={
-                "frame": np.arange(ff),
-                "height": np.arange(2 * pad + hh),
-                "width": np.arange(2 * pad + ww),
-            },
-            name="Y_bg",
+        A_bg = darr.from_array(
+            sparse.COO.from_numpy(np.where(A_bg > zero_thres, A_bg, 0)), chunks=-1
         )
+        C_bg = darr.from_array(
+            random_walk(ff, ndim=bg_nsrc, norm=True), chunks=(chk_size, -1)
+        )
+        Y_bg = darr.tensordot(C_bg, A_bg, axes=1)
         Y = Y + Y_bg
+    Y = Y + darr.random.normal(
+        scale=0.1, size=(ff, hh + 2 * pad, ww + 2 * pad), chunks=(chk_size, -1, -1)
+    )
+    uids, hs, ws, fs = np.arange(ncell), np.arange(hh), np.arange(ww), np.arange(ff)
+    Y = xr.DataArray(
+        Y, dims=["frame", "height", "width"], coords={"frame": fs}, name="Y"
+    )
+    A = xr.DataArray(
+        A[:, pad:-pad, pad:-pad].compute().todense(),
+        dims=["unit_id", "height", "width"],
+        coords={"unit_id": uids, "height": hs, "width": ws},
+        name="A",
+    )
+    C = xr.DataArray(
+        C, dims=["unit_id", "frame"], coords={"unit_id": uids, "frame": fs}, name="C"
+    )
+    S = xr.DataArray(
+        S, dims=["unit_id", "frame"], coords={"unit_id": uids, "frame": fs}, name="S"
+    )
     Y = (
         apply_shifts(Y, shifts)
-        .compute()
         .isel(height=slice(pad, -pad), width=slice(pad, -pad))
+        .assign_coords(height=hs, width=ws)
+        .rename("Y")
     )
-    Y = (Y + random.normal(scale=0.1, size=(ff, hh, ww))).rename("Y")
-    return (
-        Y,
-        A.isel(height=slice(pad, -pad), width=slice(pad, -pad)),
-        C,
-        S,
-        shifts,
-    )
+    return Y, A, C, S, shifts
 
 
-def generate_data(dpath, **kwargs):
+def generate_data(
+    dpath, save_Y=False, post_offset: float = None, post_gain: float = None, **kwargs
+):
     Y, A, C, S, shifts = simulate_data(**kwargs)
-    Y = (
-        ((Y - Y.min()) / (Y.max() - Y.min()) * 255)
-        .astype(np.uint8)
-        .chunk({"frame": 500})
-    )
-    for dat in [Y, A, C, S, shifts]:
+    if (post_offset is not None) and (post_gain is not None):
+        Y = (Y + post_offset) * post_gain
+    else:
+        Ymax = Y.max().compute()
+        Ymin = Y.min().compute()
+        Y = (Y - Ymin) / (Ymax - Ymin) * 255
+    Y = Y.clip(0, 255).astype(np.uint8)
+    datls = [A, C, S, shifts]
+    if save_Y:
+        datls.append(Y)
+    for dat in datls:
         save_minian(dat, dpath=dpath, overwrite=True)
     write_video(Y, os.path.join(dpath, "simulation.mp4"))
 
@@ -203,7 +209,7 @@ if __name__ == "__main__":
     generate_data(
         dpath="simulated_data",
         ncell=100,
-        dims={"height": 256, "width": 256, "frame": 1000},
+        dims={"height": 256, "width": 256, "frame": 2000},
         sig_scale=1,
         sz_mean=3,
         sz_sigma=0.6,
@@ -214,4 +220,6 @@ if __name__ == "__main__":
         bg_nsrc=100,
         mo_stps=[-2, -1, 0, 1, 2],
         mo_pstp=[0.02, 0.08, 0.8, 0.08, 0.02],
+        post_offset=1,
+        post_gain=50,
     )
