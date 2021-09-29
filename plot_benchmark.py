be9c6ab44480d596e0a3f27e02970b9b4586e612
phildong <35715936+phildong@users.noreply.github.com>
2021-09-29 12:21:15 -0400

    feat: generate publication quality plots
    * switch to using seaborn for simulated validation plots
    * switch to seaborn for benchmark plotting
    * change figure size and save png

--- plot_benchmark.py
+++ plot_benchmark.py
@@ -8,10 +8,14 @@ env: environments/generic.yml
 import os
 import re
 
+import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.express as px
+import seaborn as sns
+from matplotlib.ticker import EngFormatter
 
-IN_DPATH = "./data"
+from routine.plotting import ax_tick, format_tick
+
+IN_DPATH = "./data/simulated/benchmark"
 IN_PROF_FILES = {"minian": "minian_prof.csv", "caiman": "caiman_prof.csv"}
 IN_DIR_PATTERN = r"fm(?P<nfm>[0-9]+)-cell(?P<ncell>[0-9]+)"
 FIG_BENCH_ALL = "./fig/benchmark/overall"
@@ -48,23 +52,82 @@ for root, dirs, files in os.walk(IN_DPATH):
 prof_df = pd.concat(df_ls, ignore_index=True)
 
 #%% plot overall performance
+ASPECT = 1.2
+SMALL_SIZE = 9
+MEDIUM_SIZE = 10
+BIG_SIZE = 11
+sns.set(
+    rc={
+        "figure.figsize": (3.98, 3.98 / ASPECT),
+        "figure.dpi": 500,
+        "font.family": "sans-serif",
+        "font.sans-serif": ["Helvetica"],
+        "font.size": MEDIUM_SIZE,
+        "axes.titlesize": MEDIUM_SIZE,
+        "axes.labelsize": MEDIUM_SIZE,  # size of faceting titles
+        "xtick.labelsize": SMALL_SIZE,
+        "ytick.labelsize": SMALL_SIZE,
+        "legend.fontsize": MEDIUM_SIZE,
+        "figure.titlesize": BIG_SIZE,
+        "legend.edgecolor": "gray",
+        # "axes.linewidth": 0.4,
+        # "axes.facecolor": "white",
+        "xtick.major.size": 2,
+        "xtick.major.width": 0.4,
+        "xtick.minor.visible": True,
+        "xtick.minor.size": 1,
+        "xtick.minor.width": 0.4,
+        "ytick.major.size": 2,
+        "ytick.major.width": 0.4,
+        "ytick.minor.visible": True,
+        "ytick.minor.size": 1,
+        "ytick.minor.width": 0.4,
+    }
+)
 id_vars = ["pipeline", "nfm", "ncell"]
 val_vars = ["duration", "max_mem"]
-facet_dim = "ncell"
+metric_dict = {"duration": "Running Time (minutes)", "max_mem": "Peak Memory (MB)"}
+pipeline_dict = {"minian": "Minian", "caiman": "CaImAn"}
+
+
+def rename_axis(data, **kwargs):
+    ax = plt.gca()
+    ax.set_ylabel(metric_dict[data.iloc[0]["variable"]])
+
+
 prof_agg = (
     prof_df.groupby(id_vars)
     .agg({"duration": "sum", "max_mem": "max"})
     .reset_index()
     .astype({"ncell": int, "nfm": int})
     .sort_values(["pipeline", "ncell", "nfm"])
+    .replace({"pipeline": pipeline_dict})
 )
-for variable in val_vars:
-    fig = px.line(
-        prof_agg,
-        x="nfm",
-        y=variable,
-        color="pipeline",
-        facet_col=facet_dim,
-        markers=True,
-    )
-    fig.write_image(os.path.join(FIG_BENCH_ALL, "{}.pdf".format(variable)))
+prof_agg["duration"] = prof_agg["duration"] / 60
+prof_agg = prof_agg.melt(id_vars=id_vars)
+fig = sns.FacetGrid(
+    prof_agg,
+    row="variable",
+    col="ncell",
+    margin_titles=True,
+    legend_out=True,
+    aspect=ASPECT,
+    sharey="row",
+)
+fig.map_dataframe(
+    sns.lineplot,
+    x="nfm",
+    y="value",
+    hue="pipeline",
+    style="pipeline",
+    markers=True,
+    legend="full",
+)
+fig.map_dataframe(rename_axis)
+fig.map_dataframe(ax_tick, x_var="nfm")
+fig.map(format_tick, x_formatter=EngFormatter(), y_formatter=EngFormatter())
+fig.add_legend()
+fig.set_xlabels("Frame Number")
+fig.set_titles(row_template="", col_template="{col_name} cells")
+fig.savefig(os.path.join(FIG_BENCH_ALL, "master.svg"))
+fig.savefig(os.path.join(FIG_BENCH_ALL, "master.png"))
