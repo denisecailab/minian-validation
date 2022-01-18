8d1989539a6dd35d718353f2208dbe5acc75490c
phildong <zhe.dong@icahn.mssm.edu>
2022-01-17 20:16:57 -0500

    accept specification of axis in plotting utilities

--- routine/plotting.py
+++ routine/plotting.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
 
 
-def ax_tick(data, x_var=None, y_var=None, **kwargs):
-    ax = plt.gca()
+def ax_tick(data, x_var=None, y_var=None, ax=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
     if x_var is not None:
         ax.set_xticks(data[x_var].unique())
     if y_var is not None:
         ax.set_yticks(data[y_var].unique())
 
 
-def format_tick(x_formatter=None, y_formatter=None, **kwargs):
-    ax = plt.gca()
+def format_tick(x_formatter=None, y_formatter=None, ax=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
     if x_formatter is not None:
         ax.xaxis.set_major_formatter(x_formatter)
     if y_formatter is not None:
         ax.yaxis.set_major_formatter(y_formatter)
 
 
-def it_lab(**kwargs):
-    ax = plt.gca()
+def it_lab(ax=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
     ax.set_xlabel(ax.get_xlabel(), fontstyle="italic")
     ax.set_ylabel(ax.get_ylabel(), fontstyle="italic")
